6cae97bd7871a777f85660c428061b9213fd452d
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2020-08-07 18:06:18 +0530

    FSD06620B109

--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -1,2 +1,34 @@
 package service;
 
+public class PersonalityCalculator {
+	
+	public String findYourBrainType(String options) {
+		int value[] = findAnswers(options);
+
+		int a = value[0]+value[1]+value[2]+value[4]+value[7]+value[9]+value[10]+value[11]+value[13]+value[17]+value[19];
+		int b = value[3]+value[5]+value[6]+value[8]+value[12]+value[14]+value[15]+value[16]+value[18];
+		int sum = 66-a+b;
+		System.out.println("score"+sum);
+		if(sum>=20 && sum<=54)
+			return "leftbrained";
+		else if(sum>=55 && sum<=64)
+			return "No clear preference";
+		else if(sum>=65 && sum<=100)
+			return "rightbrained";
+		else
+			return "false";		
+	}
+	
+	public int[] findAnswers(String options) {
+		// TODO Auto-generated method stub
+		String [] str = options.split(",");
+		int [] array = new int[str.length];
+		for(int i=0;i<array.length;i++) {
+			array[i] = Integer.parseInt(str[i]);
+			
+		}
+		return array;
+	}
+	
+	
+}
